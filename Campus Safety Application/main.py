b2af79dcf745900e434204610dcf69b5190ddb63
tanvirA25 <113075133+tanvirA25@users.noreply.github.com>
2024-08-02 22:22:39 -0400

    Update main.py

--- Campus Safety Application/main.py
+++ Campus Safety Application/main.py
@@ -1,6 +1,16 @@
-from website import app_init
+from website import app_create, db
 
-app = app_init()
+# gets the app from init file
+app = app_create()
 
+# application context creates the database
+with app.app_context():
+    db.create_all()
+       
+       
+# run the app
 if __name__ == '__main__':
+
     app.run(debug=True)
+    
+
