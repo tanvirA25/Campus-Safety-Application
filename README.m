0091a7e1cfd90fe6a0c13bb2a787607fae7d2e26
tanvirA25 <113075133+tanvirA25@users.noreply.github.com>
2024-09-09 20:30:27 -0400

    Create README.m

---
+++ README.m
@@ -0,0 +1,19 @@
+Campus Safety Application is designed for user to get emergency alerts through a notification system. 
+
+The application have many pages to have functional and useful interactive software for user to use.
+
+
+Home Page: The home page serves as the entry point for users, providing essential navigation options. Its clean and straightforward design makes it easy for users to understand the platform's purpose and explore various sections. It includes clear call-to-action buttons, possibly guiding users toward registration, login, or learning more about the platform's services.
+
+Login and Signup Pages: The login page provides a secure area for returning users to access their accounts, while the signup page enables new users to create an account. Both pages are designed with user convenience in mind, featuring a minimalistic form structure to streamline the login and registration processes. Additional options such as "Forgot Password" are likely available to assist users with account recovery.
+
+Account Page: Account displays user information such as name, and email address.
+
+Incident Report Page: This page is only access by the administrator to update alerts with alert name, date, time, location, short description, and long description. 
+
+
+User Feedback: The feedback page emphasizes user experience by allowing individuals to provide suggestions or rate their experiences with the platform. This section is vital for continuous improvement, ensuring the developers can adjust the platform based on real user input. The feedback system demonstrates the platform’s commitment to user satisfaction.
+
+After a alert gets updated by the adminsitarotr all the user of the applciation gets an email with the following, alert name as subject, date, time, and short descrtipiton, along with location.
+
+Whole project is done using python especifically pyhton FLASK, and SQL Alchemy for database.
